aeab299fbec3d8a9fa46b699bc37c542057f342d
tobirodrigo <tobiasrodrigo2020@gmail.com>
2023-08-24 19:16:49 -0300

    cambios

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// variables y objetos necesarios
+
 let numbers = [];
 let result = 0;
 
-// Funciones para realizar operaciones
 function suma(a, b) {
     return a + b;
 }
@@ -23,20 +22,17 @@ function division(a, b) {
     }
 }
 
-// entradas mediante prompt()
 const num1 = parseFloat(prompt("Ingrese el primer número:"));
 const num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
-//  operaciones
+
 numbers.push(num1, num2);
 
 result = suma(num1, num2);
 
-// Efectuar una salida
 console.log("Números ingresados:", numbers);
 console.log("Resultado de la suma:", result);
 
-// Métodos de búsqueda y filtrado sobre el Array
 const searchValue = parseFloat(prompt("Ingrese un número para buscar en el array:"));
 if (numbers.includes(searchValue)) {
     console.log(`El número ${searchValue} está en el array.`);
