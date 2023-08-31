995784721bb7fec53d5ffb6867a4144f63eb4290
tobirodrigo <tobiasrodrigo2020@gmail.com>
2023-08-31 19:03:56 -0300

    Descripción de los cambios realizados

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
 
-let numbers = [];
-let result = 0;
+class Calculator {
+    constructor() {
+        this.numbers = [];
+        this.result = 0;
+    }
 
-function suma(a, b) {
-    return a + b;
-}
+    add(a, b) {
+        return a + b;
+    }
 
-function resta(a, b) {
-    return a - b;
-}
+    subtract(a, b) {
+        return a - b;
+    }
 
-function multiplicacion(a, b) {
-    return a * b;
-}
+    multiply(a, b) {
+        return a * b;
+    }
 
-function division(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return "No se puede dividir por cero";
+    divide(a, b) {
+        if (b !== 0) {
+            return a / b;
+        } else {
+            return "No se puede dividir por cero";
+        }
+    }
+
+    addNumberToHistory(number) {
+        this.numbers.push(number);
+    }
+
+    performSum(a, b) {
+        this.result = this.add(a, b);
+    }
+
+    searchNumberInHistory(value) {
+        if (this.numbers.includes(value)) {
+            return `El número ${value} está en el array.`;
+        } else {
+            return `El número ${value} no está en el array.`;
+        }
     }
 }
+
+const calculator = new Calculator();
 
 const num1 = parseFloat(prompt("Ingrese el primer número:"));
 const num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
+calculator.addNumberToHistory(num1);
+calculator.addNumberToHistory(num2);
+calculator.performSum(num1, num2);
 
-numbers.push(num1, num2);
-
-result = suma(num1, num2);
-
-console.log("Números ingresados:", numbers);
-console.log("Resultado de la suma:", result);
+console.log("Números ingresados:", calculator.numbers);
+console.log("Resultado de la suma:", calculator.result);
 
 const searchValue = parseFloat(prompt("Ingrese un número para buscar en el array:"));
-if (numbers.includes(searchValue)) {
-    console.log(`El número ${searchValue} está en el array.`);
-} else {
-    console.log(`El número ${searchValue} no está en el array.`);
-}
+console.log(calculator.searchNumberInHistory(searchValue));
